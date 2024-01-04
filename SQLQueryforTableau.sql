31b9d548a5b4c0559b335b26f4aa4b942045dccc
peteralahmar <46577263+peteralahmar@users.noreply.github.com>
2024-01-04 15:15:53 -0500

    Add files via upload

---
+++ SQLQueryforTableau.sql
@@ -0,0 +1,37 @@
+--1.
+
+--Getting the Total Cases and total Deaths Due to Covid-19. Using simple math, get the Death Percentage as of December 2023.
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent, such as removing income levels and international, EU and World
+
+Select Location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International','High Income', 'Upper middle income', 'Lower middle income','Low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+-- Breakdown of each conntinent into their countries(location)
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
